64f1aff403f6682cc4df4cc112f119e64aa3461f
John Fisher <jkfisher97@comcast.net>
2019-08-11 14:39:52 -0600

    Added more UI unit tests

--- app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
+++ app/src/androidTest/java/com/example/notekeeper/NoteCreationTest.java
@@ -4,6 +4,7 @@ import androidx.test.espresso.ViewInteraction;
 import androidx.test.rule.ActivityTestRule;
 import androidx.test.runner.AndroidJUnit4;
 
+import org.junit.BeforeClass;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,24 +16,59 @@ import static androidx.test.espresso.matcher.ViewMatchers.*;
 import static androidx.test.espresso.action.ViewActions.*;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 
+import static org.hamcrest.Matchers.*;
+import static androidx.test.espresso.Espresso.pressBack;
+import static androidx.test.espresso.assertion.ViewAssertions.*;
+
+
 @RunWith(AndroidJUnit4.class)
 public class NoteCreationTest {
+
+    static DataManager sDataManager;
+    @BeforeClass
+    public static void classSetUp() throws Exception{
+        sDataManager = DataManager.getInstance();
+    }
 
     @Rule
     public ActivityTestRule<NoteListActivity> mNoteListActivityRule = new ActivityTestRule<>(NoteListActivity.class);
 
     @Test
     public void createNewNote(){
-        /*
-        //get to the view
-        ViewInteraction fabNewNote = onView(withId(R.id.fab));
 
-        //perform the action
-        fabNewNote.perform(click());
-        */
-        //simply...
+        final CourseInfo course = sDataManager.getCourse("java_lang");
+        final String noteTitle = "Test note title";
+        final String noteText = "This is the body text of the test note";
+
+        //click on the new button to create a new note
         onView(withId(R.id.fab)).perform(click());
-        onView(withId(R.id.text_note_title)).perform(typeText("Test note title"));
-        onView(withId(R.id.text_note_text)).perform(typeText("This is the body of our test note"), closeSoftKeyboard());
+
+        //interact with the spinner
+        onView(withId(R.id.spinner_courses)).perform(click());//spinners need to be clicked first
+        onData(allOf(instanceOf(CourseInfo.class), equalTo(course))).perform(click());
+
+        //make sure that the course being displayed is the course title
+        onView(withId(R.id.spinner_courses)).check(matches(withSpinnerText(
+                containsString(course.getTitle()))));
+
+        //start adding information about the note
+        onView(withId(R.id.text_note_title)).perform(typeText(noteTitle)).check(matches(withText(containsString(noteTitle))));
+
+
+        onView(withId(R.id.text_note_text)).perform(typeText(noteText), closeSoftKeyboard());
+
+        onView(withId(R.id.text_note_text)).check(matches(withText(containsString(noteText))));
+
+        //return to previous view
+        pressBack();
+
+        //make sure that the note is saved
+        int noteIndex = sDataManager.getNotes().size() - 1;
+        NoteInfo note = sDataManager.getNotes().get(noteIndex);
+        //expected, actual
+        assertEquals(course, note.getCourse());
+        assertEquals(noteTitle, note.getTitle());
+        assertEquals(noteText, note.getText());
+
     }
 }
