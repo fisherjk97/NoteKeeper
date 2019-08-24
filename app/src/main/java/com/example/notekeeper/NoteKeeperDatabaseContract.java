db267bec6e4ce7b8af7bb0bff0624894bb742742
John Fisher <fisherjk97@gmail.com>
2019-08-24 10:21:20 -0600

    Updating table schema with constraints and primary keys

--- app/src/main/java/com/example/notekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/com/example/notekeeper/NoteKeeperDatabaseContract.java
@@ -1,10 +1,12 @@
 package com.example.notekeeper;
 
+import android.provider.BaseColumns;
+
 public final class NoteKeeperDatabaseContract {
 
     private NoteKeeperDatabaseContract() {}//make non-createable
 
-    public static final class CourseInfoEntry{
+    public static final class CourseInfoEntry implements BaseColumns {
 
         public static final String TABLE_NAME = "course_info";
         public static final String COLUMN_COURSE_ID = "course_id";
@@ -13,13 +15,14 @@ public final class NoteKeeperDatabaseContract {
         //CREATE TABLE course_info (course_id, course_title)
 
         public static final String SQL_CREATE_TABLE =
-                "CREATE TABLE " + TABLE_NAME + "("
-                        + COLUMN_COURSE_ID + ", "
-                        + COLUMN_COURSE_ID  + ")";
+                "CREATE TABLE " + TABLE_NAME + "(" +
+                        _ID + " INTEGER PRIMARY KEY, "
+                        + COLUMN_COURSE_ID + " TEXT UNIQUE NOT NULL, "
+                        + COLUMN_COURSE_TITLE  + " TEXT NOT NULL)";
 
     }
 
-    public static final class NoteInfoEntry{
+    public static final class NoteInfoEntry implements BaseColumns{
 
         public static final String TABLE_NAME = "course_info";
         public static final String COLUMN_NOTE_TITLE= "note_title";
@@ -28,9 +31,10 @@ public final class NoteKeeperDatabaseContract {
 
         public static final String SQL_CREATE_TABLE =
                 "CREATE TABLE " + TABLE_NAME + "("
-                        + COLUMN_NOTE_TITLE + ", "
-                        + COLUMN_NOTE_TEXT  + ", "
-                        + COLUMN_COURSE_ID + ")";
+                        + _ID + " INTEGER PRIMARY KEY, "
+                        + COLUMN_NOTE_TITLE + " TEXT NOT NULL, "
+                        + COLUMN_NOTE_TEXT  + " TEXT, "
+                        + COLUMN_COURSE_ID + " TEXT NOT NULL)";
 
     }
 }
