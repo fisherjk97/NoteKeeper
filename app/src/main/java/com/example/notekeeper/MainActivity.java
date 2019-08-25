1f9914054ef248f8bf9a603d983f0a012996517e
John Fisher <fisherjk97@gmail.com>
2019-08-25 14:47:41 -0600

    adding table name qualifier helper

--- app/src/main/java/com/example/notekeeper/MainActivity.java
+++ app/src/main/java/com/example/notekeeper/MainActivity.java
@@ -243,9 +243,10 @@ public class MainActivity extends AppCompatActivity
                 public Cursor loadInBackground() {
                     SQLiteDatabase db = mDbOpenHelper.getReadableDatabase();
                     final String[] noteColumns = {
-                            NoteInfoEntry._ID,
+                            NoteInfoEntry.getQName(NoteInfoEntry._ID),
                             NoteInfoEntry.COLUMN_NOTE_TITLE,
-                            NoteInfoEntry.COLUMN_COURSE_ID};
+                            NoteInfoEntry.getQName(NoteInfoEntry.COLUMN_COURSE_ID),
+                            CourseInfoEntry.COLUMN_COURSE_TITLE};
                     final String noteOrderBy = NoteInfoEntry.COLUMN_COURSE_ID +
                             "," + NoteInfoEntry.COLUMN_NOTE_TITLE;
 
@@ -253,8 +254,8 @@ public class MainActivity extends AppCompatActivity
 
                     String tablesWithJoin = NoteInfoEntry.TABLE_NAME  + " JOIN " +
                             CourseInfoEntry.TABLE_NAME + " ON " +
-                            NoteInfoEntry.TABLE_NAME + "." + NoteInfoEntry.COLUMN_COURSE_ID + " = " +
-                            CourseInfoEntry.TABLE_NAME + "." + CourseInfoEntry.COLUMN_COURSE_ID;
+                            NoteInfoEntry.getQName(NoteInfoEntry.COLUMN_COURSE_ID)+ " = " +
+                            CourseInfoEntry.getQName(CourseInfoEntry.COLUMN_COURSE_ID);
 
 
                     return db.query(tablesWithJoin, noteColumns,
