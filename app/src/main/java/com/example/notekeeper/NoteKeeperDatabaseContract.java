c6686cca62d4b925881efbf9b018ad0084c37aa2
John Fisher <fisherjk97@gmail.com>
2019-08-25 15:07:10 -0600

    Upgraded database with table indexes

--- app/src/main/java/com/example/notekeeper/NoteKeeperDatabaseContract.java
+++ app/src/main/java/com/example/notekeeper/NoteKeeperDatabaseContract.java
@@ -12,6 +12,13 @@ public final class NoteKeeperDatabaseContract {
         public static final String COLUMN_COURSE_ID = "course_id";
         public static final String COLUMN_COURSE_TITLE = "course_title";
 
+        //CREATE INDEX course_info_index1 on course_info (course_title)
+        public static final String INDEX1 = TABLE_NAME + "_index_1";
+        public static final String SQL_CREATE_INDEX1 =
+                "CREATE INDEX "  + INDEX1 + " ON " + TABLE_NAME +
+                        "(" + COLUMN_COURSE_TITLE + ")";
+
+
         public static final String getQName(String columnName){
             return TABLE_NAME + "." + columnName;
         }
@@ -32,6 +39,12 @@ public final class NoteKeeperDatabaseContract {
         public static final String COLUMN_NOTE_TITLE= "note_title";
         public static final String COLUMN_NOTE_TEXT = "note_text";
         public static final String COLUMN_COURSE_ID = "course_id";
+
+        public static final String INDEX1 = TABLE_NAME + "_index_1";
+        public static final String SQL_CREATE_INDEX1 =
+                "CREATE INDEX "  + INDEX1 + " ON " + TABLE_NAME +
+                        "(" + COLUMN_NOTE_TITLE + ")";
+
 
         public static final String getQName(String columnName){
             return TABLE_NAME + "." + columnName;
--- app/src/main/java/com/example/notekeeper/NoteKeeperOpenHelper.java
+++ app/src/main/java/com/example/notekeeper/NoteKeeperOpenHelper.java
@@ -4,10 +4,12 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import static com.example.notekeeper.NoteKeeperDatabaseContract.*;
+
 public class NoteKeeperOpenHelper extends SQLiteOpenHelper {
 
     public static final String DATABASE_NAME = "NoteKeeper.db";
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
 
 
     public NoteKeeperOpenHelper(Context context){
@@ -16,8 +18,13 @@ public class NoteKeeperOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(NoteKeeperDatabaseContract.CourseInfoEntry.SQL_CREATE_TABLE);
-        db.execSQL(NoteKeeperDatabaseContract.NoteInfoEntry.SQL_CREATE_TABLE);
+        //create table schema
+        db.execSQL(CourseInfoEntry.SQL_CREATE_TABLE);
+        db.execSQL(NoteInfoEntry.SQL_CREATE_TABLE);
+
+        //create table indexes
+        db.execSQL(CourseInfoEntry.SQL_CREATE_INDEX1);
+        db.execSQL(NoteInfoEntry.SQL_CREATE_INDEX1);
 
         DatabaseDataWorker worker = new DatabaseDataWorker(db);
         worker.insertCourses();
@@ -26,6 +33,10 @@ public class NoteKeeperOpenHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        //upgrade people on database 1 to next version
+        if(oldVersion < 2) {//evolve this overtime so we want to have this hardcoded
+            db.execSQL(CourseInfoEntry.SQL_CREATE_INDEX1);
+            db.execSQL(NoteInfoEntry.SQL_CREATE_INDEX1);
+        }
     }
 }
