1c4fff72b02a1462d2ddf94a19aeb56c27a216fe
John Fisher <fisherjk97@gmail.com>
2019-08-31 09:26:03 -0600

    Added table columns to contract class

--- app/src/main/java/com/example/notekeeper/NoteKeeperProviderContract.java
+++ app/src/main/java/com/example/notekeeper/NoteKeeperProviderContract.java
@@ -1,6 +1,7 @@
 package com.example.notekeeper;
 
 import android.net.Uri;
+import android.provider.BaseColumns;
 
 public final class NoteKeeperProviderContract {
 
@@ -9,14 +10,32 @@ public final class NoteKeeperProviderContract {
     public static final String AUTHORITY = "com.example.notekeeper.provider";
     public static final Uri AUTHORITY_URI = Uri.parse("content://" + AUTHORITY);
 
-    public static final class Courses {
+    protected interface CoursesIdColumns {
+        public static final String COLUMN_COURSE_ID = "course_id";
+
+    }
+
+    protected interface CoursesColumns {
+        public static final String COLUMN_COURSE_TITLE = "course_title";
+
+    }
+
+    protected interface NotesColumns {
+        public static final String COLUMN_NOTE_TITLE = "note_title";
+        public static final String COLUMN_NOTE_TEXT = "note_text";
+
+    }
+
+    //implements BaseColumns will help expose "_ID" for row identity
+
+    public static final class Courses implements BaseColumns, CoursesColumns, CoursesIdColumns {
         public static final String PATH = "courses";
         // content://com.example.notekeeper.provider/courses
         public static  final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
 
     }
 
-    public static final class Notes {
+    public static final class Notes implements BaseColumns, NotesColumns, CoursesIdColumns {
         public static final String PATH = "notes";
         // content://com.example.notekeeper.provider/notes
         public static  final Uri CONTENT_URI = Uri.withAppendedPath(AUTHORITY_URI, PATH);
